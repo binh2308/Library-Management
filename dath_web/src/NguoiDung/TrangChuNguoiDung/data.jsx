61250b3fe2424c91478457cf36013b5c7bbccb89
binh2308 <phuongbinh.conan@gmail.com>
2024-11-29 12:27:55 +0700

    Second commit

---
+++ dath_web/src/NguoiDung/TrangChuNguoiDung/data.jsx
@@ -0,0 +1,19 @@
+export const book = [
+  {
+    img: "./img/harry-potter.jpg",
+    Bname: "Harry Potter and the Deathly Hallows",
+    author: "J.K.Rowling",
+    BId: "abc1432",
+    Date_publish: "01-01-2010",
+    status: "Có thể mượn",
+  },
+  {
+    img: "./img/database.jpg",
+    Bname: "Fundamental of Database systems 7th Edition",
+    author: "Elmasri & Navathe",
+    BId: "abc1433",
+    Date_publish: "01-01-2020",
+    status: "Không thể mượn",
+  },
+  // Add more books as needed
+];
